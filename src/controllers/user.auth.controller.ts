5ec8171dee0d7f8feeba4f0321e1de92a63c4d4e
OLiver Guerrero Ruiz <oliver.kali.hub@gmail.com>
2021-10-24 16:54:03 -0500

    ✍️  add: env config for host mail

--- src/controllers/user.auth.controller.ts
+++ src/controllers/user.auth.controller.ts
@@ -1,17 +1,17 @@
 import { IUser } from './../interfaces/modelsInterfaces';
 import { createToken } from './../tokens/createToken';
-import  {Request, Response, NextFunction} from 'express'
+import { Request, Response, NextFunction } from 'express'
 import { User } from '../models/User'
 import { sendEmail } from '../helpers/email';
 import crypto from 'crypto';
 
-const signUp = async (req:Request, res:Response):Promise<Response> => {
+const signUp = async (req: Request, res: Response): Promise<Response> => {
     const {
         email,
         password,
     } = req.body as IUser
 
-    if(!email || !password) return res.status(400).json({
+    if (!email || !password) return res.status(400).json({
         msg: "Please, send your email and password"
     })
 
@@ -20,53 +20,53 @@ const signUp = async (req:Request, res:Response):Promise<Response> => {
         const saveUser = await newUser.save()
 
         /* Creando un token al registra el usuario */
-        const token:string = createToken(saveUser)
+        const token: string = createToken(saveUser)
         res.header('Authorization', `Bearer ${token} `)
 
         return res.status(201).json(saveUser)
 
     } catch (error) {
         console.log(error)
-        return res.status(400).json({msg: "Failed to register user"})
+        return res.status(400).json({ msg: "Failed to register user" })
     }
 }
 
-const signIn = async (req:Request, res:Response):Promise<Response> => {
+const signIn = async (req: Request, res: Response): Promise<Response> => {
     const {
         email,
         password
     } = req.body as IUser
 
-    if(!email || !password) return res.status(400).json({
+    if (!email || !password) return res.status(400).json({
         msg: "Please, send your email and password"
     })
-    
-   try {
-        const user = await User.findOne({email})
+
+    try {
+        const user = await User.findOne({ email })
 
         if (!user) return res.status(400).json({
             msg: "The user does not excist"
         })
-    
-        const isMatch:Boolean = await user.comparePassword(password)
-    
-        if(isMatch){
-            const token:string = createToken(user)
+
+        const isMatch: Boolean = await user.comparePassword(password)
+
+        if (isMatch) {
+            const token: string = createToken(user)
 
             res.header('Authorization', `Bearer ${token} `)
 
             return res.status(201).json(user)
 
-        } else{
-            return res.status(400).json({msg: 'Email or password not excist'})
-    
+        } else {
+            return res.status(400).json({ msg: 'Email or password not excist' })
+
         }
-   } catch (error) {
-        return res.status(500).json({msg: 'Internal Server Error. Please try again in a few moments. '})
-   }
+    } catch (error) {
+        return res.status(500).json({ msg: 'Internal Server Error. Please try again in a few moments. ' })
+    }
 }
 
-const signOut = (req:Request, res:Response, next:NextFunction):void => {
+const signOut = (req: Request, res: Response, next: NextFunction): void => {
     req.logOut()
 }
 
@@ -80,20 +80,20 @@ const forgotPassword = async (req: Request, res: Response): Promise<Response> =>
             await user.save({ validateBeforeSave: false })
 
             const URL = `${req.protocol}://${req.get('host')}/User/ResetPassword/${randomToken}`
-            
+
             const messsage = `Did you forget your password? click the link below to reset your password: ${URL}.\n
             If you do not want to reset your password, please ignore this email`
-            
-           try {
+
+            try {
                 await sendEmail({ email: user.email, subject: 'Reset your password', message: messsage })
                 return res.status(200).json({ msg: 'Email sent' })
-           } catch (error) {
+            } catch (error) {
                 user.passwordResetToken = undefined
                 user.passwordResetExpires = undefined
                 await user.save({ validateBeforeSave: false })
 
                 return res.status(400).json({ msg: error })
-           }
+            }
         }
 
         return res.status(404).json({ msg: 'User not found' })
@@ -112,7 +112,7 @@ const resetPassword = async (req: Request, res: Response) => {
         .createHash('sha256')
         .update(token)
         .digest('hex')
-    
+
     try {
         const user = await User.findOne({
             passwordResetToken: hashedToken,
@@ -126,7 +126,7 @@ const resetPassword = async (req: Request, res: Response) => {
         user.passwordResetExpires = undefined
         await user.save()
 
-        const newToken:string = createToken(user)
+        const newToken: string = createToken(user)
         res.header('Authorization', `Bearer ${newToken} `)
 
         return res.status(200).json({ msg: 'Password updated' })
--- src/helpers/email.ts
+++ src/helpers/email.ts
@@ -1,25 +1,22 @@
-import nodemailer from 'nodemailer'; 
+import nodemailer from 'nodemailer';
 
-export const sendEmail = async (options:any) => {
-    const transporter =  nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: 465,
-        secure: false,
-        auth: {
-          user: process.env.EMAIL_USERNAME,
-          pass:process.env.EMAIL_PASSWORD,
-        },
-        tls: {
-            ciphers:'SSLv3'
-        }
-      });
-    
-    const mailOptions = {
-        from: 'SubscriptionsGeek <subscriptionsGeek@mail.io>', 
-        to: options.email, 
-        subject: options.subject, 
-        text: options.message,
+export const sendEmail = async (options: any) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.host,
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.user,
+      pass: process.env.pass,
     }
+  });
 
-    await transporter.sendMail(mailOptions)
+  const mailOptions = {
+    from: 'SubscriptionsGeek <subscriptionsGeek@mail.io>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  }
+
+  await transporter.sendMail(mailOptions)
 }
